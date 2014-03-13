95df4c1cff267927326fd4fcdadfa75a1709e283
TJ Holowaychuk <tj@vision-media.ca>
2014-03-13 11:11:22 -0700

    improve .stringify() performance 50%
    because why not :D

--- index.js
+++ index.js
@@ -28,11 +28,21 @@ exports.parse = function(string, opts){
 
 exports.stringify = function(rows, opts){
   opts = opts || {};
-  var d = opts.delimiter || ',';
+  var delim = opts.delimiter || ',';
+  var csv = '';
 
-  return rows.map(function(row){
-    return row.map(quote).join(d);
-  }).join('\n');
+  for (var i = 0; i < rows.length; i++) {
+    var row = rows[i];
+
+    for (var j = 0; j < row.length; j++) {
+      csv += quote(row[j]);
+      if (j < row.length - 1) csv += delim;
+    }
+
+    if (i < rows.length - 1) csv += '\n';
+  }
+
+  return csv;
 };
 
 /**
